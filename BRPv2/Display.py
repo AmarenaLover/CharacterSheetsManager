3feb47ea3dfc58ec8d4561fa8ebd8abc53bcc0aa
Amarena_Lover <dawidos-91@wp,pl>
2024-03-07 18:24:49 +0100

    Implementation of the main menu: - CreateImage, CreateButton, CreateLabel, CreateMainMenuFooter classes were parameterized and can be used in all code, - Brp logo image now is hyperlink to Chaosium Inc. Website

--- BRPv2/Display.py
+++ BRPv2/Display.py
@@ -1,10 +1,12 @@
 # import ctypes
-
+import tkinter
+import webbrowser
 import os
 from PIL import Image
 from Defaults import Defaults
 import customtkinter as ctk
 from tkinter import messagebox
+
 
 class Display:
     def __init__(self):
@@ -19,11 +21,13 @@ class Display:
         if messagebox.askyesno("Wyjście", "Czy na pewno chcesz wyjść?"):
             self.main_window.destroy()
 
+    def change_colors(self):
+        ctk.set_appearance_mode("light")
+
     def display_main_menu(self):
         # HEAD PART
         CreateHeader(self.main_window,
-                     fg_color=self.main_window.fg_color,
-                     head_name="INTERAKTYWNA KARTA POSTACI").pack(fill=ctk.X)
+                     head_name="INTERAKTYWNA KARTA POSTACI").pack()
         # HEAD PART
 
         # BODY PART
@@ -31,71 +35,103 @@ class Display:
                             fg_color=self.main_window.fg_color)
         body.pack()
 
-        CreateButton(body, fg_color=self.main_window.fg_color, my_text="Wyświetl postać", my_command=blank).pack()
-        CreateButton(body, fg_color=self.main_window.fg_color, my_text="Stwórz postać", my_command=blank).pack()
-        CreateButton(body, fg_color=self.main_window.fg_color, my_text="Zmień motyw", my_command=blank).pack()
-        CreateButton(body, fg_color=self.main_window.fg_color, my_text="Wyjście", my_command=self.display_exit).pack()
+        CreateButton(body,
+                     button_text="Wyświetl postać",
+                     button_command=blank,
+                     button_frame_color=self.main_window.fg_color).pack()
+        CreateButton(body,
+                     button_text="Stwórz postać",
+                     button_command=blank,
+                     button_frame_color=self.main_window.fg_color).pack()
+        CreateButton(body,
+                     button_text="Zmień motyw",
+                     button_command=self.change_colors,
+                     button_frame_color=self.main_window.fg_color).pack()
+        CreateButton(body,
+                     button_text="Wyjście",
+                     button_command=self.display_exit,
+                     button_frame_color=self.main_window.fg_color).pack()
         # BODY PART
 
         # FOOT PART
         foot = ctk.CTkFrame(self.main_window)
-        foot.pack(side=ctk.BOTTOM,
+        foot.pack(fill=ctk.X,
                   padx=5, pady=5,
-                  expand=False,
-                  fill=ctk.X)
-        CreateImage(foot,
-                    # fg_color=foot.fg_color,
-                    img_name="BRP_logo.png").pack(padx=5, pady=5,
-                                                  ipadx=5, ipady=5,
-                                                  expand=False,
-                                                  side=ctk.LEFT)
-        (ctk.CTkLabel(foot,
-                      text="To oprogramowanie opiera się na zasadach gry udostępnionych przez Chaosium Inc. zgodnie z licencją BRP Open Game License, Version 1.0.",
-                      wraplength=220,
-                      # text_color=foot.fg_color,
-                      font=Defaults.default_text_font)
-         .pack(expand=False,
-               side=ctk.RIGHT,
-               padx=10, pady=5))
+                  side=ctk.BOTTOM)
+
+        CreateMainMenuFooter(foot,
+                             img_button_name="BRP_logo.png",
+                             img_button_command=lambda: webbrowser.open_new(
+                                 r"https://www.chaosium.com/content/FreePDFs/BRP/BRP%20SRD%20-%20V1.0.pdf"),
+                             footer_text_color=self.main_window.fg_color,
+                             footer_background_color=foot.fg_color).pack()
         # FOOT PART
 
 
 class CreateHeader(ctk.CTkFrame):
     def __init__(self, *args, head_name, **kwargs):
         super().__init__(*args, **kwargs)
-        head = ctk.CTkFrame(self)
-        head.pack(side=ctk.TOP,
-                  padx=5, pady=5,
+        self.pack(padx=5, pady=5,
                   ipadx=5, ipady=5,
-                  expand=False,
                   fill=ctk.X)
-        ctk.CTkLabel(master=head,
+        ctk.CTkLabel(master=self,
                      text=head_name,
                      font=Defaults.default_header1_font).pack(expand=True)
 
 
 class CreateButton(ctk.CTkFrame):
-    def __init__(self, *args, my_text, my_command, **kwargs):
+    def __init__(self, *args, button_text, button_command, button_frame_color, **kwargs):
         super().__init__(*args, **kwargs)
-        (ctk.CTkButton(master=self,
-                       text=my_text,
-                       command=my_command)
-         .pack(padx=5,
-               pady=5,
-               ipadx=5,
-               ipady=5))
+
+        self.configure(fg_color=button_frame_color)
+
+        ctk.CTkButton(master=self,
+                      text=button_text,
+                      command=button_command).pack(padx=5,
+                                                   pady=5,
+                                                   ipadx=5,
+                                                   ipady=5)
 
 
 class CreateImage(ctk.CTkFrame):
-    def __init__(self, *args, img_name, **kwargs):
+    def __init__(self, *args, img_name, img_command=None, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.pack(padx=5, pady=5, expand=False)
+
         img = ctk.CTkImage(light_image=Image.open(os.path.join(Defaults.default_path_images, img_name)),
                            dark_image=Image.open(os.path.join(Defaults.default_path_images, img_name)),
-                           size=(85, 85))
+                           size=(90, 90))
+
+        ctk.CTkButton(self,
+                      text="",
+                      image=img,
+                      width=10,
+                      command=img_command,
+                      fg_color=self.fg_color).pack(ipadx=5, ipady=5)
+
+
+class CreateMainMenuFooter(ctk.CTkFrame):
+    def __init__(self, *args, img_button_name, img_button_command=None, footer_text_color, footer_background_color, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.configure(fg_color=footer_background_color)
+
+        self.pack(side=ctk.BOTTOM,
+                  padx=5, pady=5)
+
+        CreateImage(self,
+                    fg_color=footer_background_color,
+                    img_name=img_button_name,
+                    img_command=img_button_command,
+                    ).pack(side=ctk.LEFT)
+
         ctk.CTkLabel(self,
-                     text="",
-                     image=img,
-                     padx=5, pady=5).pack()
+                     text="To oprogramowanie opiera się na zasadach gry udostępnionych przez Chaosium Inc. zgodnie z licencją BRP Open Game License, Version 1.0.",
+                     wraplength=600,
+                     text_color=footer_text_color,
+                     font=Defaults.default_text_font).pack(expand=True,
+                                                           padx=10, pady=5)
 
 
 # def clear_main_window():
